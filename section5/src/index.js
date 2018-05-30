43787af5bd55dd6b2b057332f19354ba45015a35
O-Junpei <jumpei.ono@bizreach.co.jp>
2018-05-30 13:49:16 +0900

    98ページまで実装した。

--- section5/src/index.js
+++ section5/src/index.js
@@ -3,6 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+const initialState = {
+  tasks : []
+};
+
+const addTask = (task) => ({
+  type: 'ADD_TASK',
+  payload: {
+    task
+  }
+});
+
+function tasksReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'ADD_TASK':
+      return {
+        ...state,
+        tasks: state.tasks.concat([action.task])
+      };
+      default:
+        return state;
+  }
+}
+
+const store = createStore(tasksReducer);
+
+store.dispatch(addTask('Storeを学ぶ'));
+
+
+console.log(store.getState());
